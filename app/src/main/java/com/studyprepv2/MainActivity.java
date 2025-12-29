6bfdbf3653a4889ccbebbfccaa4a4a5412a5b9f1
rahmounikaoutar5-code <rahmounikaoutar5@gmail.com>
2025-12-29 15:18:43 +0100

    init: add MainActivity

---
+++ app/src/main/java/com/studyprepv2/MainActivity.java
@@ -0,0 +1,11 @@
+package com.studyprepv2;
+
+/**
+ * Point de départ de l'application StudyPrepV2
+ */
+public class MainActivity {
+
+    public static void main(String[] args) {
+        System.out.println("StudyPrepV2 started");
+    }
+}
